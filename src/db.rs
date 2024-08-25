387ff12874c9902cb9b6133dc9eee60706f196ad
chisty <mchisty31@gmail.com>
2024-08-25 22:47:17 +0600

    database errors handled

--- src/db.rs
+++ src/db.rs
@@ -1,26 +1,27 @@
-use std::collections::{hash_map, HashMap};
+use std::collections::HashMap;
 
-pub struct Database{
- store:HashMap<String,String>,
+pub struct Database {
+    store: HashMap<String, String>,
 }
 
 impl Database {
-    pub fn new()->self{
-        Database{
-            store:HashMap::new(),
+    pub fn new() -> Self {
+        Database {
+            store: HashMap::new(),
         }
     }
+
+    pub fn set(&mut self, key: String, value: String) {
+        self.store.insert(key, value);
+    }
+
+    pub fn get(&self, key: &str) -> Option<&String> {
+        self.store.get(key)
+    }
+    
+    pub fn delete(&mut self, key: &str) -> Option<String> {
+        self.store.remove(key)
+    }
+    
 }
 
-pub fn set(&mut self,key:String,value:String)
-{
-    self.store.insert(key,value);
-}
-
-pub fn get(&self,key:&str)->Option<&String>{
-    self.store.get(key)
-}
-
-pub fn delete(&mut self,key:&str)->Option<&String>{
-    self.store.remove(key)
-}
